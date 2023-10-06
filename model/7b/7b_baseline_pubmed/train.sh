2f60d9e0fced3cfe78d09cce38410ab2b90d8bb2
Jinho Heo <uj07077@gmail.com>
2023-10-06 23:35:16 +0900

    feat: run script updated

---
+++ model/7b/7b_baseline_pubmed/train.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sh ../../prepare.sh
+
+nohup python3 model/llama-2-7b-pubmed-qa.py \
+	--report_to wandb \
+	--run_name llama-2-7b-pubmed-qa \
+	&
---
+++ prepare.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Prepare the environment for the build
+pip install --upgrade pip
+pip install -r requirements.txt
